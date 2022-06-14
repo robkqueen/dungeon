1c60d75bd3252503c9d7097377e58bb287b52fc1
robkqueen <107484483+robkqueen@users.noreply.github.com>
2022-06-14 14:54:44 +0100

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -11,19 +11,42 @@ namespace myTiles {
 2 . . . . . . . . . . . . . . 2 
 2 . . . . . . . . . . . . . . 2 
 2 . . . . . . . . . . . . . . 2 
-2 . . . . . . . . . . . . . 2 2 
-2 . . . . . . . . . . . . . 2 2 
-2 . . . . . . . . . . . . . 2 2 
 2 . . . . . . . . . . . . . . 2 
 2 . . . . . . . . . . . . . . 2 
-2 2 . . . . . . . . . . . . . 2 
-2 2 . . . . . . . . . . . . . 2 
-2 2 . . . . . . . . . . . . . 2 
-2 2 . . . . . . . . . . . . . 2 
-2 2 . . . . . . . . . . . . . 2 
-2 2 . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
 `, [myTiles.transparency16,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterEast1,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.greenOuterNorth2,sprites.dungeon.greenOuterEast2,sprites.dungeon.greenOuterSouth2], TileScale.Sixteen);
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`140014000208080b0b0b100b0b0b0b100b0b0b0b0b0b1001060a0a090d0d0a060a0a0a0a060a0a0a070a0a07060a0a090a0a0a060a0a0a0a060a0a0a070a0a07090a0a090a0a0a060a0a0a0a060a0a0a070a0a07090a0a090a0a0a0a0a0a0a0a060a0a0a070a0a07090a0a090a0a0a0a0a02010a060a0a0a070a0a070f0a0a03050505050503040a0a0a0a0a0c0a0a07060a0a0a0a0a0a0a0a0a070a0a0a0a0a0c0a0a07060a0a0a0a0a0a0a0a0a070a0a0a0a0a0c0a0a070b0b0e010a0a0a0a0a0a070a0a0a0a0a0c0a0a07060d0a070a0a0a0a0a0a070a0a0a0a0a0c0a0a07060a0a0a0a0a0a0a0a0a0a0a0a0a0a0a070a0a07060a0a0a0a0a0a0a0a0a0a0a0a0a0a0a070a0a07060a0a0a0a0a0a0a0a0a0a0a0a0a0a0a070a0a07060a0a070a0a0a0a0a0a0a0a0a0a0a0a0a0a0a07060a0a0408080808080808010a0a0a0a0a0a0a07060a0a0a0a0a0a0a0a0a0a070a0a0a0a0a0a0a07060a0a0a0a0a0a0a0a0a0a070a0a0a0a0a0a0a07060a0a0a0a0a0a0a0a0a0a070a0a0a0a0a0a0a070305050505050505050505050505050505050504`, img`
+22222222222222222222
+2..2...2....2...2..2
+2..2...2....2...2..2
+2..2...2....2...2..2
+2..2........2...2..2
+2..2.....22.2...2..2
+2..22222222.....2..2
+2.........2.....2..2
+2.........2.....2..2
+2222......2.....2..2
+2..2......2.....2..2
+2...............2..2
+2...............2..2
+2...............2..2
+2..2...............2
+2..222222222.......2
+2..........2.......2
+2..........2.......2
+2..........2.......2
+22222222222222222222
+`, [myTiles.transparency16,sprites.dungeon.purpleOuterNorthEast,sprites.dungeon.purpleOuterNorthWest,sprites.dungeon.purpleOuterSouthEast,sprites.dungeon.purpleOuterSouthWest,sprites.dungeon.purpleOuterSouth1,sprites.dungeon.purpleOuterWest0,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleOuterNorth0,sprites.dungeon.purpleOuterWest1,sprites.dungeon.darkGroundCenter,sprites.dungeon.purpleOuterNorth1,sprites.dungeon.purpleOuterEast1,sprites.dungeon.chestClosed,sprites.dungeon.purpleSwitchUp,sprites.dungeon.purpleOuterEast2,sprites.dungeon.purpleOuterSouth2], TileScale.Sixteen);
         }
         return null;
     })
