659d63f542b73738ede8d4186925709a9a58d6a6
robkqueen <107484483+robkqueen@users.noreply.github.com>
2022-06-14 14:19:37 +0100

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,39 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`10001000010202021002020202021002020202030a0c0d0c0d0c0d0c0d0c0d0c0d0c0d070a0e0f0e0f0e0f0e0f0e0f0e0f0e0f060a0c0d0c0d0c0d0c0d0c0d0c0d0c0d070a0e0f0e0f0e0f0e0f0e0f0e0f0e0f06110c0d0c0d0c0d0c0d0c0d0c0d0c0d070a0e0f0e0f0e0f0e0f0e0f0e0f0e0f070a0c0d0c0d0c0d0c0d0c0d0c0d0c0d070b0e0f0e0f0e0f0e0f0e0f0e0f0e0f070a0c0d0c0d0c0d0c0d0c0d0c0d0c0d060a0e0f0e0f0e0f0e0f0e0f0e0f0e0f060a0c0d0c0d0c0d0c0d0c0d0c0d0c0d060a0e0f0e0f0e0f0e0f0e0f0e0f0e0f070a0c0d0c0d0c0d0c0d0c0d0c0d0c0d060a0e0f0e0f0e0f0e0f0e0f0e0f0e0f0604080809120908080809091208080805`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . 2 2 
+2 . . . . . . . . . . . . . 2 2 
+2 . . . . . . . . . . . . . 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 2 . . . . . . . . . . . . . 2 
+2 2 . . . . . . . . . . . . . 2 
+2 2 . . . . . . . . . . . . . 2 
+2 2 . . . . . . . . . . . . . 2 
+2 2 . . . . . . . . . . . . . 2 
+2 2 . . . . . . . . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterEast1,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.greenOuterNorth2,sprites.dungeon.greenOuterEast2,sprites.dungeon.greenOuterSouth2], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
